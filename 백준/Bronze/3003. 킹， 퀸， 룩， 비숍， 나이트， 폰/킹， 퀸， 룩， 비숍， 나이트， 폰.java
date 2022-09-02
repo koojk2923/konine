4f4094cb262754c03e36a131376e8cfe60627509
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-09-02 15:28:42 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
-
+    // 필요한 말의 계수를 연산해서 반환하면 되는 단순한 문제이다.
     Scanner scanner = new Scanner(System.in);    
     int[] chess = {1,1,2,2,2,8}; // 배열의 길이 : 6
-    int[] arr = new int[6];
+    int[] arr = new int[6]; 
     for(int i=0; i<chess.length; i++){
        arr[i] = scanner.nextInt();
         System.out.println(chess[i]-arr[i]);
