b4ccd72a3dc1d917b0d0673e36f12abc66dc5c5c
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-09-07 13:57:42 +0900

    [Bronze V] Title: A+B - 3, Time: 136 ms, Memory: 13168 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    	// 10950번
+	// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();	// 입력 받을 테스트 개수 설정(전체 갯수를 설정)
+		
+		for(int i=0; i < n; i++) {
+			int a = scanner.nextInt(); // 입력 받을 정수 a
+			int b = scanner.nextInt(); // 입력 받을 정수 b
+			
+			System.out.println(a+b);
+		}
+	}
+}
