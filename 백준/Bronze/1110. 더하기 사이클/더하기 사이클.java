8d82fcd80f4d08292f065d2d1c3215f304041b5f
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-09-13 21:44:04 +0900

    [Bronze I] Title: 더하기 사이클, Time: 84 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,64 +1,9 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-    	public static void main(String[] args) {
-			
-		Scanner scanner = new Scanner(System.in);
-		
-		int N = scanner.nextInt();
-		scanner.close();
-		int copy = N;	// 처음입력된 값을 비교해야 하므로 변수를 복사(변수값 복사)
-		int count = 0;	//반복문이 몇 번 반복되었는지 세어주는 변수 count;
-		
-		while(true){
-			// N의 일의 자릿수는 새로운 수(T)의 십의자릿수
-			// N = (N % 10) * 10;
-
-			// 각 자릿수의 합 : 
-			// 10으로 나눈 나머지가 N의 각 자릿수의 합의 일의 자릿수가 T의 일의 자릿수
-			// N = ((N / 10) + (N % 10)) % 10
-			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10); 
-			count++;
-			
-			if(copy == N) {
-				break;
-			}
-		}
-		System.out.println(count);
-
-	}
-	
-	/*
-	 * 1110번
-	 * 더하기 사이클 (두번째 방법)
-	 */
-	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		int N = scanner.nextInt();
-		scanner.close();
-		
-		int copy = N;	// 들어온 입력값을 저장해야 한다.
-		int count = 0;	// 반복문 몇 번 반복이 되었는지 세어주는 변수;
-		
-		// do-while문
-		do {
-			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
-			count++;
-			System.out.println(N);
-		} while(copy != N);
-		
-		System.out.println(count);
-	}
-	
-		/*
-	 * 1110번
-	 * 더하기 사이클 (세번째 방법)
-	 */
-	
-	public static void main(String[] args) throws IOException {
+    	public static void main(String[] args) throws IOException {
 		 /*
 		  * BufferedReader
 		  * 입력 스트림에서 문자를 읽는 함수
@@ -84,6 +29,4 @@ public class Main{
 		
 		System.out.println(count);
 	}
-	
-	
-} // end of class
+}
