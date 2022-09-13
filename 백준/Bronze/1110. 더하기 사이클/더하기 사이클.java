0a33a879b5d529016e1b87d60a5e2ad8783aee73
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-09-13 21:39:55 +0900

    더하기싸이클+(세번째방법)
    BufferReader 사용

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -53,5 +53,37 @@ public class Main{
 		System.out.println(count);
 	}
 	
+		/*
+	 * 1110번
+	 * 더하기 사이클 (세번째 방법)
+	 */
+	
+	public static void main(String[] args) throws IOException {
+		 /*
+		  * BufferedReader
+		  * 입력 스트림에서 문자를 읽는 함수
+		  * 문자나 배열, 라인들을 효율적으로 읽기 위해 문자들을 버퍼에 저장하고 (버퍼링) 읽는 방법을 취한다.
+		  */
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		/*
+		 * readLine()
+		 * 데이터 라인 단위로 읽기
+		 * return값이 String으로 고정되기 때문에 String이 아닌 다른 타입으로 입력 받으려면 형변환을 해줘야한다.
+		 * text를 줄 단위로 읽어들인다.
+		 */
+		int N = Integer.parseInt(br.readLine());
+		
+		int copy = N;
+		int count = 0;
+		
+		do {
+			N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+			count++;
+		} while(copy != N);
+		
+		System.out.println(count);
+	}
+	
 	
 } // end of class
