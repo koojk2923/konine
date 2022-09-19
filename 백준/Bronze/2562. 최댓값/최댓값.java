cffd99e32699f767c00ff1450e511de718afaaa5
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-09-19 23:09:05 +0900

    [Bronze III] Title: 최댓값, Time: 76 ms, Memory: 11468 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,31 @@
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		
+		// *BufferedReader 은 기본 반환값이 문자이므로 반드시 Integer.parseInt()로 int 형으로 변환시켜준다.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int[] arr = new int[9];
-		int max = 0;
-		int index = 0;
 		
 		for(int i=0; i < 9; i++) {
-			arr[i] = scanner.nextInt();
+			arr[i] = Integer.parseInt(br.readLine());
 		}
-		for(int i=0; i < 9; i++) {
-			if(arr[i] > max) {
-				max = arr[i];
-				index = (i+1);
+		
+		int max = 0;
+		int count = 0;
+		int index = 0;
+		for(int value : arr) {
+			count++;
+			if(value > max) {
+				max = value;
+				index = count;
 			}
 		}
-		System.out.print(max + "\n" + index);
+		System.out.println(max);
+		System.out.println(index);
 	}
 }
