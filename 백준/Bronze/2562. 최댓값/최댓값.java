671d24f5f65566baa4f01dfe59bb324c950a91fa
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-09-19 22:33:04 +0900

    [Bronze III] Title: 최댓값, Time: 112 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,26 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
-				in.nextInt(), in.nextInt(), in.nextInt(),
-				in.nextInt(), in.nextInt(), in.nextInt() };
-		in.close();
+	public static void main(String[] args) {	
+		Scanner scanner = new Scanner(System.in);
+	
+		int[] arr = { scanner.nextInt(), scanner.nextInt(), scanner.nextInt(),
+				scanner.nextInt(), scanner.nextInt(), scanner.nextInt(),
+				scanner.nextInt(), scanner.nextInt(), scanner.nextInt() };
+		scanner.close();
 		
+		int max = 0; // 가장 큰수 담을 변
 		int count = 0;
-		int max = 0;
 		int index = 0;
-        
+		 	
+		//향상된 for문의 주의점은 배열변수만 사용이 가능하다는 점입니다!
 		for(int value : arr) {
 			count++;
-            
+			
 			if(value > max) {
 				max = value;
 				index = count;
 			}
 		}
-		System.out.print(max + "\n" + index);
-		
-	}
+		System.out.println(max + "\n" + index);
+			
+			}
 }
