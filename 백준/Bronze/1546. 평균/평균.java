1773b6539c45ae0225cb9da9efe4e62e99fbdf5a
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-10-03 15:29:07 +0900

    [Bronze I] Title: 평균, Time: 152 ms, Memory: 15752 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    
+    public static void main(String[] argrs) {
+		Scanner scanner = new Scanner(System.in);
 
-     	Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		int [] arr = new int[N];
+		double max = 0;
+		double sum = 0;
+		double avg = 0.0;
 		
-		int N = scanner.nextInt();		// 시험 본 과목의 개수 
-		double[] arr = new double[N]; 	// 과목의 개수만큼 배열길이 생성
-		
-		for(int i=0; i<arr.length; i++) {	// 과목의 점수를 입력하기 위해서 배열의 길보다 작게 해서 for문을 돌려준다.
-			arr[i] = scanner.nextInt();		// i=0 부터 돌려야 인덱스 0번부터 값을 넣어주기 때문에 i=0으로 설정
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+			sum = sum + arr[i];
 		}
 		scanner.close();
 		
-		double sum = 0;
-		// Arrays패키지에 있는 sort()를 사용하여 정렬(오름차순으로 정렬이 되고 최댓값을 배열의 마지막 원소이다.)
-		Arrays.sort(arr);  
-		for(int i = 0; i < arr.length; i++) {
-				sum = sum + ((arr[i] / arr[arr.length -1]) * 100);	// 각 점수를 최대값으로 나누어준다. 그리고 다 더해준다.
-		}
-		System.out.println(sum / arr.length);		// 총합계 나누기 과목의 개수
-        
-        
-    }    
+		avg = 100 * sum / max / N;
+		System.out.println(avg);
+	
+    }
 }
