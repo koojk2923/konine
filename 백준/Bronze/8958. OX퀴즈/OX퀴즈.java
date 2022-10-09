406f37dea20d67dfd16df32a92e749e3a3e28585
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-10-09 10:41:06 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 13364 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 
-public class Main {
-public static void main(String[] args) {
+public class Main{
     
-    	Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
+    public static void main(String[] args) {
+	 Scanner scanner = new Scanner(System.in);
+	 int N = scanner.nextInt();			//입력 받을 전체갯수 N
+	 String[] arr = new String[N];			//입력 받은 개수 만큼 배열을 생성 (두번째로 받는 타입은 문자열)
+	 
+	 for(int i=0; i<arr.length; i++) {
+		arr[i] = scanner.next();
 		
-		String str;
-		for(int i=0; i<N; i++) {
-			str = scanner.next();
-			
-			int sum=0; int count=0;
-			for(int j=0; j<str.length(); j++) {
-				
-				if(str.charAt(j) == 'O') {
-					count++;
-					sum = sum + count;
-				} else if(str.charAt(j) == 'X') {
-					count = 0;
+	 }
+	scanner.close();
+	
+	for(int i=0; i < arr.length; i++) {
+		  
+		int cnt = 0; 	// 연속횟수
+		int sum = 0; 	// 누적합산
+		
+		for(int j=0; j<arr[i].length(); j++) {
+			// length() :  length()는 문자열의 길이를 알고자 할때 사용된다.
+			if(arr[i].charAt(j) == 'O' ) {
+				// charAt() : String으로 저장된 문자열 중에서 한 글자만 선택해서 char타입으로 변환
+				cnt++;
+				} else {
+					cnt = 0;
 				}
-			} // 두번째 for문
-            System.out.println(sum);
-		}// 첫번째 for문
- }
+			sum = sum + cnt;
+			}	// end of for
+		System.out.println(sum);
+	}	// end for j
+    }
 }
