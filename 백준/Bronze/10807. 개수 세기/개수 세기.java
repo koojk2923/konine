f88de696f756cfbf33795150bf64cb5aab898695
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-10-26 15:30:37 +0900

    Update 개수 세기.java
    개수세기 - 세번째 방법

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -4,7 +4,7 @@ public class Main {
     
     public static void main(String[] args) {
        
-        /*
+        	/*
 		 * 문제 : 개수 세기
 		 * 번호 : 10807번
 		 * 첫번째 방법 
@@ -29,7 +29,7 @@ public class Main {
         }
         System.out.println(count);
         
-        /*
+       		 /*
 		 * 문제 : 개수 세기
 		 * 번호 : 10807번
 		 * 두번째 방법 
@@ -91,6 +91,36 @@ public class Main {
 		}
 		
 		System.out.println(map);
+	    
+		/*
+		 * 문제 : 개수 세기
+		 * 번호 : 10807번
+		 * 세번째 방법 
+		 */
+
+		// hashMap , getOrDefault (Object Key, V defaultValue) 이용
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		while(st.hasMoreTokens()) {
+			int m = Integer.parseInt(st.nextToken());
+			// m을 map에 count를 1씩 증가 시킨다.
+			map.put(m, map.getOrDefault(m, 0) + 1);
+		}
+		
+		int V = Integer.parseInt(br.readLine());
+		//map에 있으면 map에 저장되어 있는 값을 출력하고 없으면 0 출력
+		if(map.containsKey(V)) {
+			System.out.print(map.get(V));
+		} else {
+			System.out.print(0);
+		}
 		
     }
 }
