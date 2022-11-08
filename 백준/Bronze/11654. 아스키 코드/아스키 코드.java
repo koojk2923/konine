f26c3296f9cf0bab33b184c5ee2da92239193603
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-11-08 17:53:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 12804 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		/*
+		 * 문제 : 아스키코드
+		 * 번호 : 11654번
+		 * 첫번째 방법
+		 */
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int ch = scanner.nextLine().charAt(0);
+		
+		System.out.println(ch);
+    }
+}
