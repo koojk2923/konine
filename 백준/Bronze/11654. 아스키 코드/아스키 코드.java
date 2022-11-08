96a8395f558ba422f00f6fc69454a4774c9785b6
koojk2923 <85625243+koojk2923@users.noreply.github.com>
2022-11-08 17:55:25 +0900

    Update 아스키 코드.java
    두번째 방법

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -15,5 +15,15 @@ public class Main {
 		int ch = scanner.nextLine().charAt(0);
 		
 		System.out.println(ch);
+		
+		/*
+		 * 문제 : 아스키코드
+		 * 번호 : 11654번
+		 * 두번째 방법
+		 */
+		
+		// System.in은 byte 값으로 문자 핸 개만 읽으면서 해당 문자에 대응되는 아스키코드 값을 저장할 수 있다.
+		int a = System.in.read();
+		System.out.println(a);
     }
 }
